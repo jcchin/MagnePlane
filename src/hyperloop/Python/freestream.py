33e25b17ac99772e298f40b14d2f3ea09910ef76
Jeff Chin <jchin89@gmail.com>
2016-05-31 16:41:45 -0400

    working on top level Group

--- src/hyperloop/Python/freestream.py
+++ src/hyperloop/Python/freestream.py
@@ -8,14 +8,14 @@ from openmdao.core.group import Group
 # --- Local Python imports
 
 class Freestream(Component):
-    ''' OpenMDAO component for calculating freestream properties '''
+    ''' OpenMDAO component for calculating freestream total properties '''
 
     def __init__(self):
         super(Freestream, self).__init__()
 
         # -----------------------------------
         # --- Initialize Input Parameters ---
-        # -----------------------------------        
+        # -----------------------------------
         self.add_param('M',  0.8,   desc='freestream Mach number', units='m**2')
         self.add_param('Ts', 291.,  desc='freestream static pressure', units='K')
         self.add_param('Ps', 3500., desc='freestream static temperature', units='Pa')
@@ -37,29 +37,29 @@ class Freestream(Component):
         # ---------------------------------
         u['Ds'] = p['Ps'] / (p['R']*p['Ts'])
         u['Pt'] = p['Ps'] / ((1+(p['G']-1)/2*p['M']**2)**(-p['G']/(p['G']-1)))
-        u['Tt'] = p['Ts'] / ((1+(p['G']-1)/2*p['M']**2))**-1  
+        u['Tt'] = p['Ts'] / ((1+(p['G']-1)/2*p['M']**2))**-1
         u['Dt'] = u['Pt'] / (p['R']*u['Tt'])
 
         print("-------------------------------------")
-        print(" --- Freestream Static Conditions ---")      
+        print(" --- Freestream Static Conditions ---")
         print("-------------------------------------")
         print("MN = %.6f "                         % (p['M']))
-        print("Ps = %.3f Pa     :  = %.6f psi"     % (p['Ps'], p['Ps']*0.000145038)) 
+        print("Ps = %.3f Pa     :  = %.6f psi"     % (p['Ps'], p['Ps']*0.000145038))
         print("Ts = %.4f K      :  = %.4f R"       % (p['Ts'], p['Ts']*1.8))
         print("Ds = %.6f kg/m^3 :  = %.6f lb/ft^3" % (u['Ds'], u['Ds']*0.0624279606))
         print (" ")
         print("-------------------------------------")
-        print(" --- Freestream Total Conditions ----")      
+        print(" --- Freestream Total Conditions ----")
         print("-------------------------------------")
         print("Pt = %.3f Pa     :  = %.3f psi"     % (u['Pt'], u['Pt']*0.000145038))
         print("Tt = %.4f K      :  = %.4f R"       % (u['Tt'], u['Tt']*1.8))
-        print("Dt = %.6f kg/m^3 :  = %.6f lb/ft^3" % (u['Dt'], u['Dt']*0.0624279606)) 
+        print("Dt = %.6f kg/m^3 :  = %.6f lb/ft^3" % (u['Dt'], u['Dt']*0.0624279606))
 
 if __name__ == "__main__":
-    
+
     # -------------------------
     # --- Default Test Case ---
-    # ------------------------- 
+    # -------------------------
     p = Problem(root=Group())
     p.root.add('comp', Freestream())
     p.setup()
