8a37ed2c8754f9ac8920ac8a11ab4e8c289b1a21
goldanguyen <gonguyen96@gmail.com>
2016-06-21 17:00:50 -0400

    motor

--- src/hyperloop/Python/BasicMotorPython.py
+++ src/hyperloop/Python/BasicMotorPython.py
@@ -17,7 +17,7 @@ class BasicMotor(Component):
         super(BasicMotor, self).__init__()
 
         #Designated inputs
-        self.add_param('Torque', val=500.0, desc='Output torque', units='N-m')
+        self.add_param('Torque', val=1000.0, desc='Output torque', units='ft-lb')
         self.add_param('Max_RPM', val=4600.0, desc='max rpm of motor', units='rpm')
         self.add_param('DesignPower', val=8000.0, desc='Design value of motor', units='hp')
 
@@ -25,24 +25,23 @@ class BasicMotor(Component):
         self.add_param('Kv', val=0.1, desc='Speed/volt', units='rad/s/V')
         self.add_param('Speed', val=50.0, desc='Output shaft mechanical speed', units='rad/s')
         self.add_param('imax', val=500.0, desc='Max motor phase current', units='A')
-        self.add_param('PolePairs', val=6, desc='Number of pole pairs in motor', units='none')
+        self.add_param('PolePairs', val=6, desc='Number of pole pairs in motor', units='none') #f=w*PP/2*pi
         self.add_param('Inductance', val=0.0, desc='Motor inductance', units='Henrys')
         self.add_param('kappa', val=0.6, desc='Base speed/max speed', units='none')
         self.add_param('LDratio', val=1.5, desc='Length to diameter ratio of motor', units='none')
-        self.add_param('R0', val=0.2, desc='Motor Phase Internal Resistance at 0degC', units='Ohms')
+        self.add_param('R0', val=0.2, desc='Motor Phase Internal Resistance at 0degC', units='Ohms') #total internal resistance
         self.add_param('I0', val=0.0, desc='Motor No-load current', units='A')
         self.add_param('I0_Des', val=0.0, desc='Motor No-load Current at Nbase', units='A')
         self.add_param('nphase', val=3, desc='Number of motor phases', units='none')
 
 
         #Desired outputs
-        self.add_output('Current', val=0.0, desc='AC `input current magnitude', units='Amps')
-        self.add_output('maxCurrent', val=0.0, desc='Max input current',units='Amps')
+        self.add_output('Current', val=2.0, desc='AC `input current magnitude', units='Amps')
         self.add_output('Voltage', val=500.0, desc='AC voltage across motor', units='V')
         self.add_output('Mass', val=0.0, desc='Mass of motor', units='')
         self.add_output('Volume',val=0.0,desc='Volume of motor modeled as a cylinder',units='L')
         self.add_output('D2L',val=0.0,desc='D-squared*L parameter which is ~ to Torque',units='mm^3')
-
+        self.add_output('maxCurrent',val=0.0,desc='Max Current',units='Amps')
         #Additional outputs
         self.add_output('Resistance', val=0.0, desc='Resistance of stator', units='Ohms')
         self.add_output('Tmax', val=0.0, desc='Max Torque', units='N-m')
@@ -97,7 +96,7 @@ class BasicMotor(Component):
     def Current_calc(self,Torque,Kv):
         #Calculates current from input Torque
         Kt = .73756214837/Kv
-        Current = Torque/Kt
+        Current = Torque*Kt
         return Current
 
     def Tmax_calc(self,DesignPower,Max_RPM,kappa):
@@ -110,7 +109,7 @@ class BasicMotor(Component):
     def maxCurrent_calc(self,Speed,imax,Current,I0,I0_Des,R0,Kv,Tmax):
         #Calculates max Current from max Torque
         #w = Speed*2*numpy.pi/60.0 #RPM to rad/s
-        w=Speed
+        w=Speed*2*numpy.pi/60.0
         Kv = (imax - I0_Des)/Tmax * (30.0/numpy.pi)
         Kt = (30.0/numpy.pi) * (1.0/Kv)
         maxCurrent = Tmax*1.355818/Kt + I0
@@ -118,13 +117,13 @@ class BasicMotor(Component):
         return maxCurrent
     
     def D2L_calc(self,Tmax):
-        D2L = 293722.0*((Tmax)**(0.7592))
+        D2L = 293722.0*(Tmax**0.7592) #mm^3
         return D2L
 
     def R_calc(self,imax,D2L,LDratio,nphase):
         As = 688.7*imax
-        Dbase = (D2L/LDratio)**(1.0/3.0)/1000
-        Tph = As*3.14159 * Dbase / imax / nphase / 2.0 #Calculate number of coil turns required
+        Dbase = ((D2L/LDratio)**(1/3))/1000 #meters
+        Tph = As*numpy.pi * Dbase / imax / nphase / 2.0 #Calculate number of coil turns required
         rpert = 48.8387296964863*imax**(-1.00112597971171) #Calculate resistance per km per turn
         lm = Dbase * 3.14159 #Calculate length of a single winding
         Rpturn = lm * rpert / 1000.0 #Calculate total resistance per turn
@@ -133,8 +132,8 @@ class BasicMotor(Component):
 
     def Voltage_calc(self,Speed,PolePairs,Inductance,Current,Kv,Resistance):
         Frequency = Speed*PolePairs/(2*numpy.pi)
-        inductorImpedance = Frequency*Inductance 
-        speedVoltage = Speed/Kv
+        inductorImpedance = Frequency*Inductance #j (j=sqrt(-1))
+        speedVoltage = Kv*Speed
         resistorVoltage = Current*Resistance
         inductorVoltage = Current*inductorImpedance #*j
         realVoltage = speedVoltage + resistorVoltage
@@ -142,15 +141,15 @@ class BasicMotor(Component):
         return Voltage
     
     def Size_calc(self,LDratio,D2L):
-        Dbase = (D2L/LDratio)**(1.0/3.0)/1000
-        Lbase = LDratio*Dbase
-        Volume = (numpy.pi/4)*(Dbase**2)*Lbase
+        Dbase = ((D2L/LDratio)**(1/3))/1000  # meters
+        Lbase = LDratio*Dbase #meters
+        Volume = numpy.pi*Lbase*(Dbase/2)**2
         return Volume
 
     def Mass_calc(self,DesignPower,LDratio,D2L):
         Pmax = DesignPower*745.7
-        Dbase = (D2L/LDratio)**(1.0/3.0)/1000
-        Mass = 0.0000070646*D2L**0.9386912061
+        Dbase = ((D2L/LDratio)**(1/3))/1000  # meters
+        Mass = 0.0000070646*(D2L**0.9386912061)
         return Mass
 
 
@@ -164,7 +163,7 @@ if __name__== '__main__':
     print ('Current: %f' %prob['comp.Current'])
     print ('Max Current [A]: %f' %prob['comp.maxCurrent'])
     print ('Max Voltage [V]: %f' %prob['comp.Voltage'])
-    print ('Motor Size (Volume) [L]: %f' %prob['comp.Volume'])
+    print ('Motor Size (Volume) [m^3] [L]: %f' %prob['comp.Volume'])
     #print ('Max Torque: %f' %prob['comp.Tmax'])
     print ('Motor Size (D^2*L) [mm^3]: %f x10e-6' %prob['comp.D2L'])
     print ('Motor Weight: %f [kg]' %prob['comp.Mass'])
