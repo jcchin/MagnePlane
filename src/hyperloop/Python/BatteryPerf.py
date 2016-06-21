9f40fd7d6633f974f148dae4e340e68d823ff0f8
gssakib <gssakib>
2016-06-21 15:01:42 -0400

    1) BatteryP.py 2)BatteryPerf.py 3)BatteryWeight.py looks exactly like C++ code. Port is still unverified.

--- src/hyperloop/Python/BatteryPerf.py
+++ src/hyperloop/Python/BatteryPerf.py
@@ -54,20 +54,20 @@ class Battery_perf(Component):
     def __init__(self):
         '''input params '''
         super(Battery_perf,self).__init__()
-        self.add_param('N_cells', val=378.0, desc='Number of cells in the battery Stack', units='cells')
-        self.add_param('N_parallel', val= 100.0, desc='Number of cells in parallel in the battery stack', units='cells')
-        self.add_param('Exp_zone_amp', val=0.284, desc='Voltage lost over the exponential zone of battery', units='Volts') #A in modeling plan
-        self.add_param('Exp_zone_time_const', val=1.171, desc='Time constant for exponential zone of discharge curve', units='Amp-hours^-1') #B in modeling plan
-        self.add_param('Polarization_voltage', val=0.0036, desc='Voltage lost due to polarization', units='Volts') #K in modeling plan
-        self.add_param('Capacity', val=8.0, desc='Nominal capacity of one cell', units='Amp-hours')
+        self.add_param('N_cells', val=1.0, desc='Number of cells in the battery Stack', units='cells')
+        self.add_param('N_parallel', val= 1.0, desc='Number of cells in parallel in the battery stack', units='cells')
+        self.add_param('Exp_zone_amp', val=1.0, desc='Voltage lost over the exponential zone of battery', units='Volts') #A in modeling plan
+        self.add_param('Exp_zone_time_const', val=1.0, desc='Time constant for exponential zone of discharge curve', units='Amp-hours^-1') #B in modeling plan
+        self.add_param('Polarization_voltage', val=1.0, desc='Voltage lost due to polarization', units='Volts') #K in modeling plan
+        self.add_param('Capacity', val=12.5, desc='Nominal capacity of one cell', units='Amp-hours')
         self.add_param('No_load_voltage', val=1.28, desc='No-load constant voltage of the battery', units='Volts')#V_0 in modeling plan
-        self.add_param('Resistance', val=0.035, desc='Internal resistance of one cell', units='Ohms')
-        self.add_param('State_of_charge', val=100.0, desc='Percent of charge left in battery', units='percentage')
-        self.add_param('Old_State_of_charge', val=100.0, desc='Percent of charge left in battery', units='percentage')
+        self.add_param('Resistance', val=0.0046, desc='Internal resistance of one cell', units='Ohms')
+        self.add_param('State_of_charge', val=100., desc='Percent of charge left in battery', units='percentage')
+        self.add_param('Old_State_of_charge', val=100., desc='Percent of charge left in battery', units='percentage')
         self.add_param('timeStep', val=0.05, desc='Time step for model', units='minutes')
-        self.add_param('k_Peukert', val=1.01193, desc='Peukert Coefficient', units='none')
-        self.add_param('Current', val=0.0, desc='Current drain applied to the battery', units='Amps')
-        self.add_param('Voltage', val=0.0, desc='Voltage output of battery based on current draw', units='Volts')
+       # self.add_param('k_Peukert', val=1.01193, desc='Peukert Coefficient', units='none')
+        self.add_param('Current', val=1000., desc='Current drain applied to the battery', units='Amps')
+        self.add_param('Voltage', val=1.0, desc='Voltage output of battery based on current draw', units='Volts')
 
 
 
@@ -83,7 +83,7 @@ class Battery_perf(Component):
         Resistance = params['Resistance']
         State_of_charge = params['State_of_charge']
         Old_State_of_charge =  params['Old_State_of_charge']
-        k_Peukert = params['k_Peukert']
+
         timeStep = params['timeStep']
 
         #outputs
@@ -91,11 +91,11 @@ class Battery_perf(Component):
         Voltage = params['Voltage']
 
 
-        params['Voltage'] = self.voltage_calc(Current,N_cells, N_parallel,Old_State_of_charge,k_Peukert, Capacity,No_load_voltage, Polarization_voltage,Exp_zone_amp,Exp_zone_time_const, Resistance, timeStep)
+        params['Voltage'] = self.voltage_calc(Current,N_cells, N_parallel,Old_State_of_charge, Capacity,No_load_voltage, Polarization_voltage,Exp_zone_amp,Exp_zone_time_const, Resistance, timeStep)
 
 
 
-    def voltage_calc(self,Current,N_cells, N_parallel, Old_State_of_charge,k_Peukert, Capacity,No_load_voltage, Polarization_voltage,Exp_zone_amp,Exp_zone_time_const, Resistance, timeStep):
+    def voltage_calc(self,Current,N_cells, N_parallel, Old_State_of_charge, Capacity,No_load_voltage, Polarization_voltage,Exp_zone_amp,Exp_zone_time_const, Resistance, timeStep):
         CurrBatt = Current / N_parallel #the capacity of the battery
         Capacity_1 = (Old_State_of_charge / 100) * Capacity #Capacity remaining in series, the time dependent instantaneous charge of the battery
 
@@ -106,7 +106,7 @@ class Battery_perf(Component):
 
         VoltageBatt = No_load_voltage - (Polarization_voltage * (1 / s)) + Exp_zone_amp * (numpy.e ** ( -Exp_zone_time_const * (1 - s)*Capacity )) - (Resistance * CurrBatt)
 
-        State_of_charge = ((Capacity_1 - (timeStep) * CurrBatt**k_Peukert) / Capacity) * 100
+        State_of_charge = (Capacity_1 - timeStep /60* CurrBatt) / Capacity * 100
 
         #Total Voltage (voltage adds in series)
         Voltage = N_cells /N_parallel * VoltageBatt
