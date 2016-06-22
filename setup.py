93d6fdee5eacd28c602ab771bb99f92827a1934a
andipeng <andi.peng@nasa.gov>
2016-06-22 10:34:41 -0400

    edited build error

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='hyperloop',
 	version = '2.0',
-	author = 'NASA MARTI'
+	author = 'NASA MARTI',
 	packages = find_packages('src'),
 	package_dir = {'': 'src'},
 	install_requires = [pycycle, openmdao, scipy, matplotlib, numpy],
