5d3b9199d48184e0ba79a84e0840be23597e58b0
kennethdecker <kennethdecker@email.arizona.edu>
2016-06-29 09:20:57 -0400

    Documented solve_nonlinear methods

--- src/hyperloop/Python/propulsion_mechanics.py
+++ src/hyperloop/Python/propulsion_mechanics.py
@@ -78,7 +78,47 @@ class PropulsionMechanics(Component):
 
     def solve_nonlinear(self, params, unknowns, resids):
         """Evaluate function Preq = (1/eta)*(mg*(vf-vo)+(1/6)*(Cd*rho*S*(vf^3 - vo^3)))
-        Can be optimized in the future.  Friction and magnetic drag are neglected for now."""
+        Can be optimized in the future.  Friction and magnetic drag are neglected for now.
+
+        Params
+        ------
+        Tube Pressure : float
+            Pressure of air in tube.  Default value is 100 Pa.  Value will come from vacuum component
+        Ideal Gas Constant : float
+            Ideal gas constant. Default valut is 287 J/(m*K).
+        Ambient Temperature : float
+            Tunnel ambient temperature. Default value is 298 K.
+        Gravity : float
+            Gravitational acceleration. Default value is 9.81 m/s**2
+        Top Speed : float
+            Top pod speed after boosting section. Default value is 335 m/s. Value will be taken from aero module
+        Entance speed : float
+            Speed of pod when it enters boosting section. Default value is 324 m/s.
+        Magnet density : float
+            Permanent magnet density. Default value is 7400 kg/m**3
+        Area of magnets : float
+            Area of magnets on bottom of pod. Default value is .0225 m**2. Value will come from levitation module
+        Thickness of magnets : float
+            Magnet thickness on bottomm of pod. Default value is .05 m. Value will come from levitation module
+        Pod mass : float
+            total mass of pod. Default value is 3100 kg. Value will come from weight component
+        Efficiency : float
+            Efficiency of propulsion system. Default value is .8. value will come from propulsion module.
+        Drag coefficient : float
+            Drag coefficient of pod.  Default value is .2. More accurate results will come from CFD
+        Reference Area : float
+            Reference area of the pod. Default value is 1.4 m**2. Value will be pulled from geometry module
+        Magnetic Drag : float
+            Drag force from magnetic levitation in N. Default value is 150 N.  Value will come from levitation analysis
+        Pod Thrust : float
+            Thrust produced by pod compressed air. Default value 3500 N. Will pull value from NPSS
+
+        Returns
+        -------
+        Required Power : float
+            Computes power required by accelerating segment
+
+        """
 
         eta = params['eta']
         g = params['g']
@@ -88,7 +128,7 @@ class PropulsionMechanics(Component):
         S = params['S']
 
         #Calculate intermediate variables
-        rho = params['p_tube']/(params['R']*params['T_ambient'])         #Calculate air density, rho = P/(RT)
+        rho = params['p_tube']/(params['R']*params['T_ambient'])            #Calculate air density, rho = P/(RT)
         m = params['m_pod'] + params['rho_pm']*params['A']*params['t']      #Calculate total mass, m = m_pod + rho*A*t
         L = ((vf**2)-(v0**2))/(2*g)                                         #Calculate necessary track length
 
