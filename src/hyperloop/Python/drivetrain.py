d1e067cd96a75822731cfbe9d88ee2a0dac84c3c
cxsummer <colin.x.summer@nasa.gov>
2016-07-01 17:42:04 -0700

    drivetrain finished, just needs to be validated/tested

--- src/hyperloop/Python/drivetrain.py
+++ src/hyperloop/Python/drivetrain.py
@@ -4,13 +4,37 @@ from hyperloop.Python.electric_motor import ElectricMotor
 from hyperloop.Python.inverter import Inverter
 from hyperloop.Python.dc_transformer import DCTransformer
 from hyperloop.Python.battery import Battery
+import numpy as np
+
+# from openmdao.api.PP import PetscKSP.
 
 
 class Drivetrain(Group):
+    # TODO improve these comments...
+    """The `Drivetrain` group represents a `Group` of an electric motor, inverter and battery
+    in an OpenMDAO model.
+
+    Models the performance and behavior of a combined electric motor, battery, and inverter
+    following previous work from [1]_
+
+    Components
+    ----------
+    Motor : ElectricMotor
+        Represents a BLDC electric motor
+    Inverter : Inverter
+        Represents an Inverter
+    Battery : Battery
+        Represents a Battery
+
+    References
+    ----------
+    .. [1] Gladin, Ali, Collins, "Conceptual Modeling of Electric and Hybrid-Electric Propulsion for UAS Applications"
+       Georgia Tech, 2015
+
+    """
+
     def __init__(self):
         super(Drivetrain, self).__init__()
-
-        # self.add('InputVoltage', IndepVarComp('Voltage', 500.0))
 
         self.add('Motor', ElectricMotor())
         self.add('Inverter', Inverter())
@@ -19,18 +43,24 @@ class Drivetrain(Group):
         # connect ElectricMotor outputs to Inverter inputs
         self.connect('Motor.frequency', 'Inverter.output_frequency')
         self.connect('Motor.phase_voltage', 'Inverter.output_voltage')
+        self.connect('Motor.phase_voltage', 'Inverter.input_voltage')
+        # TODO VERIFY THIS, does this make sense to force inverter to have
+        # equal input/output voltage?
         self.connect('Motor.phase_current', 'Inverter.output_current')
 
         # connect Inverter outputs to Battery inputs
         self.connect('Inverter.input_current', 'Battery.des_current')
         self.connect('Inverter.input_power', 'Battery.des_power')
 
+        # TODO remove this, represented previous non-converging cycle
         # connect Battery outputs to Inverter inputs
         # self.connect('Battery.output_voltage', 'Inverter.input_voltage')
 
         self.nl_solver = NLGaussSeidel()
-        self.nl_solver.options['atol'] = 1.0e-12
+        self.nl_solver.options['atol'] = 0.1
+        self.nl_solver.options['maxiter'] = 200
         self.ln_solver = ScipyGMRES()
+        # self.ln_solver = PetscKSP()
 
 
 if __name__ == '__main__':
@@ -38,8 +68,6 @@ if __name__ == '__main__':
     import sqlitedict
     from openmdao.api import SqliteRecorder
     from pprint import pprint
-
-
 
     top = Problem()
     top.root = Drivetrain()
@@ -49,21 +77,36 @@ if __name__ == '__main__':
     rec.options['record_metadata'] = True
     top.driver.add_recorder(rec)
 
-
     top.setup()
 
+    # top['Battery.n_parallel'] = 0
     # # Setting initial values for design variables
     # top['InputVoltage.Voltage'] = 200.0
 
     top.run()
-
-    db = sqlitedict.SqliteDict('drivetraindb', 'openmdao')
-    data = db['rank0:Driver/1']
-
-    pprint(data['Parameters'])
     print
+    print('bat out v: %f ' % top['Battery.output_voltage'])
+    print('invert in v: %f' % top['Inverter.input_voltage'])
+    print('mot in volt: %f' % top['Motor.phase_voltage'])
+    print('invert out volt: %f' % top['Inverter.output_voltage'])
     print
-    pprint(data['Unknowns'])
-    top.cleanup()
-    remove('drivetraindb')
-
+    print('invert in pow %f' % top['Inverter.input_power'])
+    in_pow = top['Inverter.input_voltage'] * top['Inverter.input_current']
+    print('calc inverter in pow %f' % in_pow)
+    output_power = top['Inverter.output_voltage'] * top[
+        'Inverter.output_current'] * 3.0 * np.sqrt(2.0 / 3.0)
+    print('calc output pow %f' % output_power)
+    print('bat des pow: %f' % top['Battery.des_power'])
+    print
+    print('Inv in cur %f' % top['Inverter.input_current'])
+    # print('ncells %f' % top['Battery'])
+    #
+    # db = sqlitedict.SqliteDict('drivetraindb', 'openmdao')
+    # pprint(db.keys())
+    # data = db['rank0:Driver/1']
+    # pprint(data['Parameters'])
+    # print
+    # print
+    # pprint(data['Unknowns'])
+    # top.cleanup()
+    # remove('drivetraindb')
--- src/hyperloop/Python/inverter.py
+++ src/hyperloop/Python/inverter.py
@@ -3,40 +3,90 @@ from openmdao.api import Component
 
 
 class Inverter(Component):
+    """The `Inverter` class represents a BLDC inverter in an OpenMDAO model
+
+    The `Inverter` class models the efficiency loss across a typical BLDC
+    inverter following the example from [1]_.
+
+    Params
+    ------
+    efficiency : float
+        power out / power in (W)
+    output_voltage : float
+        amplitude of AC output voltage (A)
+    output_current : float
+        amplitude of AC output current (A)
+    output_frequency : float
+        frequency of AC output (Hz)
+    input_voltage : float
+        amplitude of DC input voltage (V)
+
+    Outputs
+    -------
+    input_current : float
+        amplitude of DC input current (A)
+    input_power : float
+        amplitude of DC input current (W)
+
+    References
+    ----------
+    .. [1] Gladin, Ali, Collins, "Conceptual Modeling of Electric and Hybrid-Electric Propulsion for UAS Applications"
+       Georgia Tech, 2015
+    """
+
     def __init__(self):
+        """Initializes a `Inverter` object
+
+        Sets up the given Params/Outputs of the OpenMDAO `Inverter` component, initializes their shape, and
+        sets them to their default values.
+        """
         super(Inverter, self).__init__()
 
         self.add_param('efficiency', 1.0, desc='power out / power in')
         self.add_param('output_voltage',
                        120.0,
-                       desc='Amplitude of AC output voltage',
+                       desc='amplitude of AC output voltage',
                        units='V')
         self.add_param('output_current',
                        2.0,
-                       desc='Amplitude of AC output current',
+                       desc='amplitude of AC output current',
                        units='A')
         self.add_param('output_frequency',
                        60.0,
-                       desc='Frequency of AC output',
+                       desc='frequency of AC output',
                        units='Hz')
         self.add_param('input_voltage',
-                       28.46,
-                       desc='Amplitude of DC input voltage',
+                       100.,
+                       desc='amplitude of DC input voltage',
                        units='V')
-
 
         self.add_output('input_current',
                         0.48,
-                        desc='Amplitude of DC input current',
+                        desc='amplitude of DC input current',
                         units='A')
         self.add_output('input_power', 10.0, units='W')
 
     def solve_nonlinear(self, params, unknowns, resids):
+        """Runs the `Battery` component and sets its respective outputs to their calculated results
+
+        Args
+        ----------
+        params : `VecWrapper`
+            `VecWrapper` containing parameters
+
+        unknowns : `VecWrapper`
+            `VecWrapper` containing outputs and states
+
+        resids : `VecWrapper`
+            `VecWrapper` containing residuals
+
+        output_power = params['output_voltage'] * params[
+            'output_current'] * 3.0 * np.sqrt(2.0 / 3.0)
+        """
         output_power = params['output_voltage'] * params[
             'output_current'] * 3.0 * np.sqrt(2.0 / 3.0)
 
         # TODO perform efficiency lookup
         unknowns['input_power'] = output_power / params['efficiency']
-        unknowns['input_current'] = unknowns['input_power'] / params['input_voltage']
-        print unknowns['input_current']
-        print params['input_voltage']
+        unknowns['input_current'] = unknowns['input_power'] / params[
+            'input_voltage']
