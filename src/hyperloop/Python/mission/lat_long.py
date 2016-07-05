cfdf58936bc8ae5db17688f9a6a9007e869666d1
andipeng <andi.peng@nasa.gov>
2016-07-05 15:43:25 -0400

    revised lat_long and terrain with Rob edits

--- src/hyperloop/Python/mission/lat_long.py
+++ src/hyperloop/Python/mission/lat_long.py
@@ -2,31 +2,34 @@ from __future__ import print_function
 
 import numpy as np
 from openmdao.api import IndepVarComp, Component, Group, Problem
+from pointer.components import EOMComp
 
-class LatLong(Component):
+class LatLong(EOMComp):
 
-	def __init__(self):
-		super(LatLong, self).__init__()
+	def __init__(self, grid_data, Re=6378.137, lon_origin=-121.0, lat_origin=35.0):
+		super(LatLong, self).__init__(grid_data, time_units='s')
 
-		self.add_param('x', val=0.0, desc='X-component in N-E-D frame', units='km')
-		self.add_param('y', val=0.0, desc='Y-component in N-E-D frame', units='km')
-		self.add_param('long_origin', val=-121.0, desc='Longitude of window origin', units='deg')
-		self.add_param('lat_origin', val=35.0, desc='Latitude of window origin', units='deg')
-		self.add_param('R_E', val=6378, desc='Radius of the Earth', units='km')
+		nn = grid_data['num_nodes']
 
-		self.add_output('lat', val=0.0, desc='Latitude at input coordinate', units='deg')
-		self.add_output('long', val=0.0, desc='Longitude at an input coordinate', units='deg')
+		self.add_param('x', val=np.zeros(nn), desc='X-component in N-E-D frame', units='km')
+		self.add_param('y', val=np.zeros(nn), desc='Y-component in N-E-D frame', units='km')
+
+		self._Re = Re
+		self._lon_origin = lon_origin
+		self._lat_origin = lat_origin
+
+		self.add_output('lat', shape=(nn,), desc='Latitude at input coordinate', units='deg')
+		self.add_output('long', shape=(nn,), desc='Longitude at an input coordinate', units='deg')
 
 	def solve_nonlinear(self, p, u, r):
 		x = p['x']
 		y = p['y']
-		R_E = p['R_E']
 
-		lat_origin_rad = p['lat_origin'] * (np.pi/180.0)
-		long_origin_rad = p['long_origin'] * (np.pi/180.0)
+		lat_origin_rad = self._lat_origin * (np.pi/180.0)
+		long_origin_rad = self._lon_origin * (np.pi/180.0)
 
-		lat_rad = lat_origin_rad + (x/R_E)
-		long_rad = long_origin_rad + (y/(R_E*np.cos(lat_rad)))
+		lat_rad = lat_origin_rad + (x/self._Re)
+		long_rad = long_origin_rad + (y/(self._Re*np.cos(lat_rad)))
 
 		u['lat'] = lat_rad*(180.0/np.pi)
 		u['long'] = long_rad*(180.0/np.pi)
@@ -34,16 +37,18 @@ class LatLong(Component):
 if __name__ == '__main__':
 	top = Problem()
 	root = top.root = Group()
+	grid_data = { 'num_nodes': 1 }
 
 	params = (('x', 100.0, {'units' : 'km'}),
 			('y',100.0, {'units' : 'km'}),
 		)
 
 	root.add('input_vars', IndepVarComp(params), promotes = ['x', 'y'])
-	root.add('p', LatLong(), promotes = ['x', 'y'])
+	root.add('p', LatLong(grid_data=grid_data), promotes = ['x', 'y'])
 
 	top.setup()
 	top.run()
 
 	print('\n')
 	print('Point is at %f degrees N and %f degrees E' % (top['p.lat'], top['p.long']))
+	
