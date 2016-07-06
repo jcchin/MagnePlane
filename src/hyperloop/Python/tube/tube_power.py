8e8efd8da704e502f74de2e297b0042be807cd4f
goldanguyen <gonguyen96@gmail.com>
2016-07-06 12:23:53 -0400

    changed to match moved files

--- src/hyperloop/Python/tube/tube_power.py
+++ src/hyperloop/Python/tube/tube_power.py
@@ -1,14 +1,16 @@
-from openmdao.api import Component,Group,Problem
-
+from openmdao.api import IndepVarComp,Component,Group
+import numpy as np
 
 class TubePower(Component):
     def __init__(self):
         super(TubePower, self).__init__()
-        self.add_param('vac_power',val=1000.0,desc='Power for vacuum',units='W')
-        self.add_param('prop_power',val=300000.0,desc='Power for propulsion systems (LIM/LSM)',units='W')
-        self.add_param('tube_temp',val=0.0,desc='Tube temperature',units='K')
+        self.add_param('vac_power',val=0.0,desc='Power for vacuum',units='W')
+        self.add_param('prop_power',val=0.0,desc='Power for propulsion systems (LIM/LSM)',units='W')
 
-        #self.add_output('cooling_power',val=0.0,desc='Power for tube temperature control',units='W')
+        #self.add_param('tube_temp',val=0.0,desc='Tube temperature',units='K')
+        #self.add_param('cooling_power',val=0.0,desc='Power for tube temperature control',units='W')
+
+
         self.add_output('tot_power', val=0.0, desc='Total power output', units='W')
 
     def solve_nonlinear(self, params, unknowns, resids):
