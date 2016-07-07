2ce99e299b661afff2a0d53f07a7f191ac4db50c
goldanguyen <gonguyen96@gmail.com>
2016-07-07 16:30:26 -0400

    organized

--- src/hyperloop/Python/tube/tube_group.py
+++ src/hyperloop/Python/tube/tube_group.py
@@ -5,11 +5,11 @@ Vacuum, Tube Temperature, Tube and Pylon (structural), Propulsion Mechanics, Tub
 
 from openmdao.api import Group, Problem, IndepVarComp, ScipyGMRES
 
-from hyperloop.Python.pod.propulsion_mechanics import PropulsionMechanics
-from hyperloop.Python.tube.tube_and_pylon import TubeAndPylon
-from hyperloop.Python.tube.tube_power import TubePower
 from hyperloop.Python.tube.tube_vacuum import Vacuum
-from hyperloop.Python.tube.tube_wall_temp import TempBalance
+from hyperloop.Python.tube.tube_wall_temp import TempBalance, TubeWallTemp
+from hyperloop.Python.tube.tube_and_pylon import TubeAndPylon
+from hyperloop.Python.pod.propulsion_mechanics import PropulsionMechanics
+from hyperloop.Python.tube.tube_power import TubePower
 
 class TubeGroup(Group):
     def __init__(self):
@@ -19,30 +19,31 @@ class TubeGroup(Group):
         #self.add('Indep',IndepVarComp(params))
 
         #Adding in components to Tube Group
-        self.add('Vacuum', Vacuum(),promotes=['rad']) #need to add A_tube and calculate radius and length in vacuum
-        self.add('TempBalance', TempBalance(),promotes=['temp_boundary',
-                                                        'area_rad'])
-        self.add('Struct', TubeAndPylon(),promotes=['m_pod',
+        self.add('Vacuum', Vacuum(), promotes=['rad']) #need to add A_tube and calculate radius and length in vacuum
+        self.add('TempBalance', TempBalance(), promotes=['temp_boundary'])
+        self.add('TubeWallTemp',TubeWallTemp(), promotes=['radius_outer_tube',
+                                                         'length_tube'])
+        self.add('Struct', TubeAndPylon(), promotes=['m_pod',
                                                     'r',
                                                     'h']) #need to add A_tube and calculate radius in tubeAndPylon
-        self.add('PropMech', PropulsionMechanics(),promotes=['A',
+        self.add('PropMech', PropulsionMechanics(), promotes=['A',
                                                              't',
                                                              'm_pod',
                                                              'Cd',
                                                              'S',
                                                              'D_magnetic',
                                                              'Thrust_pod'])
-        self.add('TubePower', TubePower(),promotes=[''])
+        self.add('TubePower', TubePower())
 
-        #Connects vacuum outputs
+        #Connects vacuum outputs to downstream components
         self.connect('Vacuum.weighttot', 'Struct.vac_weight')  # need to add Vac_weight to tube_and_pylon
         self.connect('Vacuum.totpwr', 'TubePower.vac_power')
 
-        #Connects tube_wall_temp outputs
-        self.connect('TempBalance.temp_boundary', 'PropMech.T_ambient')
+        #Connects tube_wall_temp outputs to downstream components
+        self.connect('temp_boundary', 'PropMech.T_ambient')
         #self.connect('TempBalance.temp_boundary', 'TubePower.tube_temp')
 
-        #Connects propulsion_mechanics outputs
+        #Connects propulsion_mechanics outputs to downstream components
         self.connect('PropMech.pwr_req', 'TubePower.prop_power')
 
         self.ln_solver = ScipyGMRES()
@@ -62,7 +63,6 @@ if __name__ == "__main__":
     print('Struct.vac_weight: %f' % top['Struct.vac_weight'])
     print('Vacuum.totpwr %f' % top['Vacuum.totpwr'])
     print('TubePower.vac_power: %f' %top['TubePower.vac_power'])
-    print('Struct.dT_tube: %f' %top['Struct.dT_tube'])
     print('PropMech.pwr_req: %f' %top['PropMech.pwr_req'])
     print('TubePower.prop_power: %f' %top['TubePower.prop_power'])
 
