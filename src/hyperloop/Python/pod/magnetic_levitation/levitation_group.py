74dab3effdc41b80d941b5ed9c04eef5e536d375
andipeng <andi.peng@nasa.gov>
2016-07-07 10:22:42 -0400

    revised levitation group (still need to add track conditions)

--- src/hyperloop/Python/pod/magnetic_levitation/levitation_group.py
+++ src/hyperloop/Python/pod/magnetic_levitation/levitation_group.py
@@ -3,17 +3,22 @@ Group containing the breakpointlev.py classes Drag and Mass
 """
 
 from openmdao.api import Group, Problem, IndepVarComp
-
-from Python.pod.magnetic_levitation.breakpoint_levitation import BreakPointDrag, MagMass
-
+from breakpoint_levitation import BreakPointDrag, MagMass
+from magnetic_drag import MagDrag
 
 class LevGroup(Group):
     def __init__(self):
         super(LevGroup, self).__init__()
 
         # Creates components of the group.
-        self.add('Drag', BreakPointDrag())
-        self.add('Mass', MagMass())
+        self.add('Drag', BreakPointDrag(), promotes=['m_pod', 'w_track', 'l_pod', 'w_mag'])
+        self.add('Mass', MagMass(), promotes=['l_pod', 'w_mag', 'm_mag', 'cost'])
+        self.add('MDrag', MagDrag(), promotes=['mag_drag'])
+
+        self.connect('Drag.track_res', 'MDrag.track_res')
+        self.connect('Drag.track_ind', 'MDrag.track_ind')
+        self.connect('Drag.lam', 'MDrag.lam')
+        self.connect('Drag.fyu', 'MDrag.fyu')        
 
 if __name__ == "__main__":
 
@@ -22,26 +27,21 @@ if __name__ == "__main__":
     root = top.root = Group()
 
     # Define Parameters
-    params = (('m_pod', .375, {'units': 'kg'}), ('l_pod', 25.0, {'units': 'm'}),
-              ('w_track', 2.0, {'units': 'm'}), ('vel_b', 23.0, {'units': 'm/2'}),
+    params = (('m_pod', .375, {'units': 'kg'}), 
+    		  ('l_pod', 25.0, {'units': 'm'}),
+              ('w_track', 2.0, {'units': 'm'}), 
+              ('vel_b', 23.0, {'units': 'm/s'}),
               ('w_mag', 2.0, {'units': 'm'}))
 
     root.add('input_vars', IndepVarComp(params))
     root.add('lev', LevGroup())
 
-    root.connect('input_vars.m_pod', 'lev.Drag.m_pod')
-
-    root.connect('input_vars.l_pod', 'lev.Drag.l_pod')
-    root.connect('input_vars.l_pod', 'lev.Mass.l_pod')
-
-    root.connect('input_vars.w_mag', 'lev.Drag.w_mag')
-    root.connect('input_vars.w_mag', 'lev.Mass.w_mag')
+    root.connect('input_vars.m_pod', 'lev.m_pod')
+    root.connect('input_vars.l_pod', 'lev.l_pod')
+    root.connect('input_vars.w_mag', 'lev.w_mag')
+    root.connect('input_vars.vel_b', 'lev.Drag.vel_b')
 
     top.setup()
     top.run()
 
-    # print('l_pod Drag is %f' % top['lev.Drag.l_pod'])
-    # print('l_pod Mass is %f' % top['lev.Mass.l_pod'])
-    # print('\n')
-    # print('w_mag from Drag is %f' % top['lev.Drag.w_mag'])
-    # print('w_mag from Mass is %f' % top['lev.Mass.w_mag'])
+    print('Mag_drag %fN' % top['lev.mag_drag'])
