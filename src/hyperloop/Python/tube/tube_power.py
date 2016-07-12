9c820b53bb28cb28b6a837cc41edd5cbba87c295
goldanguyen <gonguyen96@gmail.com>
2016-07-12 15:52:46 -0400

    documentation

--- src/hyperloop/Python/tube/tube_power.py
+++ src/hyperloop/Python/tube/tube_power.py
@@ -2,11 +2,40 @@ from openmdao.api import IndepVarComp,Component,Group
 import numpy as np
 
 class TubePower(Component):
+    """
+    Computes the total power requirement for all tube components:
+    Vacuum, TubeAndPylon, PropulsionMechanics
+
+    Params
+    ------
+    vac_power : float
+        Power requirement to run vacuum pumps (W)
+    prop_power : float
+        Power requirement to accelerate pod (W)
+    tube_temp : float
+        Ambient temperature inside tube (K)
+
+    Outputs
+    -------
+    cooling_power : float
+        Power requirement to cool tube (W)
+    tot_power : float
+        Total power requirement for all tube components (W)
+
+    Notes
+    -----
+    Power requirement to cool the tube is not currently calculated in this component. Params to calculate
+    that power in the future are commented out for the meantime.
+
+    TODO: add in calculations for refrigeration power requirement?
+
+    """
+
     def __init__(self):
         super(TubePower, self).__init__()
         self.add_param('vac_power',val=0.0,desc='Power for vacuum',units='W')
         self.add_param('prop_power',val=0.0,desc='Power for propulsion systems (LIM/LSM)',units='W')
-        #self.add_param('tube_temp',val=0.0,desc='Tube temperature',units='K')
+        self.add_param('tube_temp',val=0.0,desc='Tube temperature',units='K')
         #self.add_output('cooling_power',val=0.0,desc='Power for tube temperature control',units='W')
         self.add_output('tot_power', val=0.0, desc='Total power output', units='W')
 
