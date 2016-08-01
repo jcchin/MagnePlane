af5c6304cb1ef05dfe7b93259a7f7656978ef4f1
kennethdecker <kennethdecker@email.arizona.edu>
2016-08-01 18:27:15 -0400

    added sweet docs

--- src/hyperloop/Python/sample_mission.py
+++ src/hyperloop/Python/sample_mission.py
@@ -8,6 +8,51 @@ class SampleMission(Component):
 	Notes
 	-------
 	This component outputs relevant mission parameters assuming a flat trajectore from LA to SF
+
+	params
+	--------
+	p_tunnel : float
+		Tunnel pressure. Default value is 850 Pa.
+	T_tunnel : float
+		Tunnel temperature. Default value is 320 K.
+	Cd : float
+		Drag coefficient. Default value is .2
+	m_pod : float
+		Pod mass. Default value is 10000.0 kg
+	D_mag : float
+		magnetic drag. Default value is (10000.0*9.81)/200.0 N
+	ram_drag : float
+		Inlet ram drag. Default value is 1855.44 N
+	nozzle_thrust : float
+		Gross thrust from the nozzle. Default value is 5503.12 N 
+	M_pod : float
+		Pod mach number. Default value is .8
+	g : float
+		Gravitational acceleration. Default value is 9.81 m/s**2
+	R : float
+		Ideal gas constant. Default value is 287 J/kg/K
+	gam : float
+		Ratio of specific heats. Dafault value is 1.4
+	theta : float
+		Elevation angle of track. Default value is 0.0 rad
+	track_length : float
+		Total length of track. Default value is 600.0e3 m
+
+	Returns
+	-------
+	dx_start : float
+		Start up distance in m
+	dx_boost : float
+		length of individual booster in m
+	boost_time : float
+		Time in individual booster section in s
+	prop_period : float
+		distance between propulsion sections in m
+	num_thrust : float
+		number of booster sections
+	coast_time : float
+		Time coasting between boosters in s
+
 	'''
 
 	def __init__(self):
@@ -19,8 +64,8 @@ class SampleMission(Component):
 		self.add_param('S', 40.0, desc = 'Pod planform area', units = 'm**2')
 		self.add_param('m_pod', 10000.0, desc = 'Pod mass', units = 'kg')
 		self.add_param('D_mag', (10000.0*9.81)/200.0, desc = 'Magnetic Drag', units = 'N')
-		self.add_param('ram_drag', 1855.44, desc = 'Pod planform area', units = 'N')
-		self.add_param('nozzle_thrust', 5503.12, desc = 'Pod planform area', units = 'N')
+		self.add_param('ram_drag', 1855.44, desc = 'Ram drag of innlet', units = 'N')
+		self.add_param('nozzle_thrust', 5503.12, desc = 'Gross thrust from the nozzle', units = 'N')
 		self.add_param('M_pod', .8, desc = 'Pod mach number', units = 'unitless')
 		self.add_param('g', 9.81, desc = 'gravity', units = 'm/s**2')
 		self.add_param('R', 287.0, desc = 'Ideal gas constant', units = 'J/(kg*K)')
@@ -32,8 +77,8 @@ class SampleMission(Component):
 		self.add_output('dx_boost', 1.0, desc = 'Booster length', units = 'm')
 		self.add_output('boost_time', 1.0, desc = 'Time on booster', units = 's')
 		self.add_output('prop_period', 1.0, desc = 'Distance between boosters', units = 'm')
-		self.add_output('num_thrust', 1.0, desc = 'Start up distance', units = 'm')
-		self.add_output('coast_time', 1.0, desc = 'Start up distance', units = 'm')
+		self.add_output('num_thrust', 1.0, desc = 'Start up distance', units = 'unitless')
+		self.add_output('coast_time', 1.0, desc = 'Start up distance', units = 's')
 
 	def solve_nonlinear(self, p, u, r):
 		p_tunnel = p['p_tunnel']
