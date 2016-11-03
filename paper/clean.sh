b5cade4c892cb96d103283284e2dee0e79fce250
colinxs <colin.x.summers@nasa.gov>
2016-11-03 00:55:16 -0700

    changed build script to run all python files located under images/{xdsm, trade_scripts} in parallel prior to building PDF changed clean script to clean all pngs as well

--- paper/clean.sh
+++ paper/clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #delete spam
-find . -regextype posix-extended -regex '.*\.(aux|log|pyg|synctex.gz|lof|lot|toc|DS_Store|out|nls|ilg|nlo|bbl|pdf)$' -delete
+find . -regextype posix-extended -regex '.*\.(aux|png|log|pyg|synctex.gz|lof|lot|toc|DS_Store|out|nls|ilg|nlo|bbl|pdf)$' -delete
 
